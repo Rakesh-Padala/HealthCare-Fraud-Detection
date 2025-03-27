40db7a1ff7d42c735d76a09978be01aee9cb2c0e
Niharika-KIDAMBI <142401114+Niharika-KIDAMBI@users.noreply.github.com>
2025-03-27 22:01:40 +0530

    Add files via upload

---
+++ untitled.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+train_label = pd.read_csv(r"C:\Users\darur\healthcare-provider-fraud-detection\Train-1542865627584.csv")
+print(train_label.head())  # Check if the file loads properly
